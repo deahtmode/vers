12ada5fccb7cce69fbd9c28a379315d1e4702e5c
Эмиль Сугаипов <emilsugaipov@gmail.com>
2015-11-18 05:36:42 +0300

    график

--- js/app.js
+++ js/app.js
@@ -1,14 +1,41 @@
 $(function() {
-
-    var newColumn = function(){
+    var startPoint;
+    var newColumn = function(direction){
         var date = new Date();
-        var random = Math.random()*149+150;
+
+        if(!direction) {
+            var random = Math.random()*149+150;
+        } else if(direction == 'up') {
+            if(!startPoint){
+                startPoint = Number($('.demo_column i').last().text());
+            }
+
+            startPoint += Math.random()*35+2;
+
+            var random = startPoint;
+
+        } else if(direction == 'down') {
+
+                if(!startPoint){
+                    startPoint = Number($('.demo_column i').last().text());
+                }
+
+                startPoint -= Math.random()*35+2;
+
+                var random = startPoint;
+
+
+        }
+
+
         if($('.demo_column').length > 7) {
             $('.demo_column:first').remove();
         };
+        var vector = (Number('79.8'+ parseInt(random)) > Number($('.demo_column i').last().text())) ? 'vector_up' : 'vector_down';
+
         $('<div class="demo_column"><span>' +
             ( date.getHours() +':'+ date.getMinutes() +':'+ date.getSeconds()) +
-            '</span><i>'+ ('79.8'+parseInt(random)) +'</i></div>')
+            '</span><i class="'+ vector +'">'+ ('79.8'+parseInt(random)) +'</i></div>')
             .css({
                 'height' : parseInt(random) + 'px',
                 'width' : '12.5%'
@@ -17,6 +44,60 @@ $(function() {
 
     };
     newColumn();
-    setInterval(newColumn, 2000);
+    var standartFactor = setInterval(newColumn, 2000);
+
+
+    $('.link_green').on('click', function(e) {
+        clearInterval(standartFactor);
+
+        $('.link_red').attr('disabled', 'disabled');
+        $('.link_green').attr('disabled', 'disabled');
+
+        e.preventDefault();
+        newColumn('up');
+        setTimeout(function(argument) {
+            newColumn('up');
+        }, 2000);
+        setTimeout(function(argument) {
+            newColumn('up');
+        }, 4000);
+        setTimeout(function(argument) {
+            newColumn('up');
+        }, 6000);
+        setTimeout(function(argument) {
+            newColumn('up');
+        }, 8000);
+        setTimeout(function(argument) {
+            newColumn('up');
+        }, 10000);
+
+        setTimeout(showPopup, 13000);
+    });
+
+
+    $('.link_red').on('click', function(e) {
+        clearInterval(standartFactor);
+
+        $('.link_red').attr('disabled', 'disabled');
+        $('.link_green').attr('disabled', 'disabled');
+        e.preventDefault();
+        newColumn('down');
+        setTimeout(function(argument) {
+            newColumn('down');
+        }, 2000);
+        setTimeout(function(argument) {
+            newColumn('down');
+        }, 4000);
+        setTimeout(function(argument) {
+            newColumn('down');
+        }, 6000);
+        setTimeout(function(argument) {
+            newColumn('down');
+        }, 8000);
+        setTimeout(function(argument) {
+            newColumn('down');
+        }, 10000);
+        setTimeout(showPopup, 13000);
+    });
 
 });
